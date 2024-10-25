4fd3269bda707fe97ca30ad735a487e0b87430ab
ivakhramov <ivan.vakhramov@outlook.com>
2024-10-25 18:53:31 +0300

    init: Ветка master, написанная на занятии 22, и которую нужно доработать в ДЗ

---
+++ client/src/main/java/ru/ivakhramov/java/basic/chat/client/ClientApplication.java
@@ -0,0 +1,11 @@
+package ru.ivakhramov.java.basic.chat.client;
+
+import java.io.IOException;
+
+public class ClientApplication {
+
+    public static void main(String[] args) throws IOException {
+
+        new Client();
+    }
+}
---
+++ server/src/main/java/ru/ivakhramov/java/basic/chat/server/ServerApplication.java
@@ -0,0 +1,10 @@
+package ru.ivakhramov.java.basic.chat.server;
+
+
+public class ServerApplication {
+
+    public static void main(String[] args) {
+
+        new Server(8189).start();
+    }
+}
